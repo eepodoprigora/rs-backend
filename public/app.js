042bf7d7e09071dcbd4f9df3ca8fa0ea2935d8a3
Евгения Подопригора <eepodoprigora@gmail.com>
2024-10-20 20:08:37 +0300

    task2 with express

--- public/app.js
+++ public/app.js
@@ -1,30 +1,94 @@
-console.log("Hello from app js");
-
 document.addEventListener("click", (e) => {
   if (e.target.dataset.type === "remove") {
     const id = e.target.dataset.id;
-    remove(id).then(() => e.target.closest("li").remove());
+    removeNote(id).then(() => e.target.closest("li").remove());
   }
-  if (e.target.dataset.type === "edit") {
-    const id = e.target.dataset.id;
-    const titleElem = e.target.closest("li").querySelector(".note-title");
-    const oldTitle = titleElem.textContent;
-    const newTitle = prompt("Enter new title", oldTitle);
 
-    if (oldTitle !== newTitle) {
-      edit(id, newTitle).then((titleElem.textContent = newTitle));
-    }
+  if (e.target.classList.contains("edit-btn")) {
+    const li = e.target.closest("li");
+    const {
+      titleElement,
+      inputElement,
+      editBtn,
+      saveBtn,
+      cancelBtn,
+      deleteBtn,
+    } = getNoteElements(li);
+
+    titleElement.style.display = "none";
+    inputElement.style.display = "block";
+    editBtn.style.display = "none";
+    saveBtn.style.display = "inline-block";
+    cancelBtn.style.display = "inline-block";
+    deleteBtn.style.display = "none";
+  }
+
+  if (e.target.classList.contains("save-btn")) {
+    const li = e.target.closest("li");
+    const {
+      titleElement,
+      inputElement,
+      editBtn,
+      saveBtn,
+      cancelBtn,
+      deleteBtn,
+    } = getNoteElements(li);
+
+    const newTitle = inputElement.value;
+
+    edit(e.target.dataset.id, newTitle).then(() => {
+      titleElement.textContent = newTitle;
+
+      inputElement.style.display = "none";
+      titleElement.style.display = "block";
+      saveBtn.style.display = "none";
+      cancelBtn.style.display = "none";
+      editBtn.style.display = "inline-block";
+      deleteBtn.style.display = "inline-block";
+    });
+  }
+
+  if (e.target.classList.contains("cancel-btn")) {
+    const li = e.target.closest("li");
+    const {
+      titleElement,
+      inputElement,
+      editBtn,
+      saveBtn,
+      cancelBtn,
+      deleteBtn,
+    } = getNoteElements(li);
+
+    inputElement.style.display = "none";
+    titleElement.style.display = "block";
+    saveBtn.style.display = "none";
+    cancelBtn.style.display = "none";
+    editBtn.style.display = "inline-block";
+    deleteBtn.style.display = "inline-block";
   }
 });
 
-async function remove(id) {
+async function removeNote(id) {
   await fetch(`/${id}`, { method: "DELETE" });
 }
 
 async function edit(id, newTitle) {
   await fetch(`/${id}`, {
     method: "PUT",
-    headers: { "Content-Type": "application/json" },
+    headers: {
+      "Content-Type": "application/json",
+    },
     body: JSON.stringify({ title: newTitle }),
   });
+}
+
+function getNoteElements(li) {
+  return {
+    titleElement: li.querySelector(".note-title"),
+    inputElement: li.querySelector(".edit-input"),
+    editBtn: li.querySelector(".edit-btn"),
+    saveBtn: li.querySelector(".save-btn"),
+    cancelBtn: li.querySelector(".cancel-btn"),
+    deleteBtn: li.querySelector(".btn-danger"),
+  };
 }
